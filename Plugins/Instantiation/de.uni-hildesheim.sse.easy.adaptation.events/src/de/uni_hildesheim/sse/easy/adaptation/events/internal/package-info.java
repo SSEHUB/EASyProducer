f986b70e04a383bed48871343a2170e1cb1009c0
Elscha <elscha@sse.uni-hildesheim.de>
2015-09-09 15:43:15 +0200

    Removed deprecated project: adaptation.events.

--- Plugins/Instantiation/de.uni-hildesheim.sse.easy.adaptation.events/src/de/uni_hildesheim/sse/easy/adaptation/events/internal/package-info.java
+++
@@ -1,6 +0,0 @@
-/**
- * Adaptation event classes that are internal to the implementation and shall not be shared.
- * 
- * @author Holger Eichelberger
- */
-package de.uni_hildesheim.sse.easy.adaptation.events.internal;
