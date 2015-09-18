42a8bf9bf9ad01d032c12e351e8349df9834793a
sassai <sassai@uni-hildesheim.de>
2015-09-18 12:40:40 +0200

    Changed formatting

--- Plugins/Instantiation/Instantiator.Java.Test/testdata/artifacts/javaCopy/test/Bla.java
+++ Plugins/Instantiation/Instantiator.Java.Test/testdata/artifacts/javaCopy/test/Bla.java
@@ -4,9 +4,7 @@ import java.util.List;
 import test.Bla2;
 
 public class Bla {
-
     private String test = "abc";
-
     private List<?> li = new a.test.veryspecial.MagicList();
 
     public String testBla() {
--- Plugins/Instantiation/Instantiator.Java.Test/testdata/artifacts/javaCopy/test/Bla2.java
+++ Plugins/Instantiation/Instantiator.Java.Test/testdata/artifacts/javaCopy/test/Bla2.java
@@ -1,9 +1,7 @@
 package test;
 
 public class Bla2 {
-
     public static String test123() {
         return "test123";
     }
-
 }
