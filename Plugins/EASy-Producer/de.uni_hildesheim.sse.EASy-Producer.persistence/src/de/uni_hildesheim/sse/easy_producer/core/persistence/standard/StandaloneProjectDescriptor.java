25ee2411dcd0f0d63a164f93511657bb4c2ee19b
eichelberger <eichelberger@sse.uni-hildesheim.de>
2016-03-26 11:22:17 +0100

    Adding the standalone project descriptor from QM-IConf for reuse

---
+++ Plugins/EASy-Producer/de.uni_hildesheim.sse.EASy-Producer.persistence/src/de/uni_hildesheim/sse/easy_producer/core/persistence/standard/StandaloneProjectDescriptor.java
@@ -0,0 +1,106 @@
+package de.uni_hildesheim.sse.easy_producer.core.persistence.standard;
+
+import java.io.File;
+
+import de.uni_hildesheim.sse.easy_producer.instantiator.model.buildlangModel.BuildModel;
+import de.uni_hildesheim.sse.easy_producer.instantiator.model.buildlangModel.Script;
+import de.uni_hildesheim.sse.easy_producer.instantiator.model.vilTypes.IProjectDescriptor;
+import de.uni_hildesheim.sse.utils.modelManagement.ModelInfo;
+import de.uni_hildesheim.sse.utils.modelManagement.ModelManagementException;
+import de.uni_hildesheim.sse.utils.progress.ProgressObserver;
+
+/**
+ * Implements a descriptor for the source and target VIL project locations.
+ * The {@link #ProjectDescriptor() no-argument constructor}
+ * obtains the relevant information for the model itself (i.e., the project
+ * it is located in). The {@link #ProjectDescriptor(StandaloneProjectDescriptor, File) 
+ * second constructor} allows to specify a different target location.
+ * 
+ * @author Holger Eichelberger
+ */
+public class StandaloneProjectDescriptor implements IProjectDescriptor {
+
+    private StandaloneProjectDescriptor parent;
+    private File base;
+    private Script vilScript;
+
+    /**
+     * Creates the standalone project descriptor.
+     * 
+     * @param vilInfo the model information instance for the VIL script to run
+     * @param base the folder to instantiate into
+     * @throws ModelManagementException in case that resolving the model, obtaining 
+     *   information etc failed.
+     */
+    public StandaloneProjectDescriptor(ModelInfo<Script> vilInfo, File base) throws ModelManagementException {
+        this.parent = null;
+        this.base = base;
+
+        vilScript = BuildModel.INSTANCE.load(vilInfo);
+    }
+
+    /**
+     * Allows to instantiate into a given location.
+     * 
+     * @param parent the parent descriptor (also representing the source, to
+     *   be obtained via {@link #ProjectDescriptor()})
+     * @param base the folder to instantiate into
+     */
+    public StandaloneProjectDescriptor(StandaloneProjectDescriptor parent, File base) {
+        this.parent = parent;
+        this.base = base;
+        if (!base.exists()) {
+            base.mkdirs();
+        }
+        this.vilScript = parent.getMainVilScript();
+    }
+    
+    @Override
+    public File getBase() {
+        return base;
+    }
+
+    @Override
+    public int getPredecessorCount() {
+        return null != parent ? 1 : 0;
+    }
+
+    @Override
+    public IProjectDescriptor getPredecessor(int index) {
+        if (index < 0 || index >= getPredecessorCount()) {
+            throw new IndexOutOfBoundsException();
+        }
+        return parent;
+    }
+
+    @Override
+    public Script getMainVilScript() {
+        return vilScript;
+    }
+
+    @Override
+    public ProgressObserver createObserver() {
+        return ProgressObserver.NO_OBSERVER; // TODO check, preliminary
+    }
+
+    @Override
+    public String getModelFolder(ModelKind kind) {
+        String result;
+        switch (kind) {
+        case IVML:
+            result = PersistenceConstants.EASY_FILES_DEFAULT;
+            break;
+        case VIL:
+            result = PersistenceConstants.EASY_FILES_DEFAULT;
+            break;
+        case VTL:
+            result = PersistenceConstants.EASY_FILES_DEFAULT;
+            break;
+        default:
+            result = null;
+            break;
+        }
+        return result;
+    }
+
+}
