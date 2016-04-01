477531fb199aeb0bd60d1be1698ae9a58d322ea2
Elscha <elscha@sse.uni-hildesheim.de>
2016-04-01 11:44:54 +0200

    Updated EASy-Producer bundle references in feature manifests
    Renamed Eclipse project name of easy.producer.help
    References #51

--- Plugins/EASy-Producer/de.uni-hildesheim.sse.easy.help/src/net/ssehub/easy/producer/help/Activator.java
+++ Plugins/EASy-Producer/de.uni-hildesheim.sse.easy.help/src/net/ssehub/easy/producer/help/Activator.java
@@ -11,7 +11,7 @@ import org.osgi.framework.BundleContext;
 public class Activator extends AbstractUIPlugin {
 
     // The plug-in ID
-    public static final String PLUGIN_ID = "de.uni-hildesheim.sse.easy.help"; //$NON-NLS-1$
+    public static final String PLUGIN_ID = "net.ssehub.easy.producer.help"; //$NON-NLS-1$
 
     // The shared instance
     private static Activator plugin;
