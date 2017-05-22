74f62bd9bae78adc8d4f7ca2d613c6aae3817643
eichelberger <eichelberger@sse.uni-hildesheim.de>
2017-05-22 15:38:43 +0200

    a global default locale

---
+++ Plugins/VarModel/Utils/src/net/ssehub/easy/basics/DefaultLocale.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright 2009-2016 University of Hildesheim, Software Systems Engineering
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package net.ssehub.easy.basics;
+
+import java.util.Locale;
+
+/**
+ * Provides access to the default locale.
+ * 
+ * @author Holger Eichelberger
+ */
+public class DefaultLocale {
+
+    public static final Locale INITIAL_LOCALE = new Locale("en", "us");
+    private static Locale defaultLocale = INITIAL_LOCALE;
+
+    /**
+     * Changes the default locale for evaluation.
+     * 
+     * @param locale the new locale, ignored if <b>null</b>
+     */
+    public static void setDefaultLocale(Locale locale) {
+        if (null != locale)  {
+            defaultLocale = locale;
+        }
+    }
+
+    /**
+     * Returns the default locale for evaluation.
+     * 
+     * @return the locale
+     */
+    public static Locale getDefaultLocale() {
+        return defaultLocale;
+    }
+
+}
