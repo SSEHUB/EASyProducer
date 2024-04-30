1231172f4351bf24350d03fce22319adf1558dc8
Holger Eichelberger <eichelberger@sse.uni-hildesheim.de>
2024-04-30 09:57:16 +0200

    migration

---
+++ Plugins/Integration/de.uni_hildesheim.sse.integration.common.tests/src/net/ssehub/easy/integration/common/tests/AllTests.java
@@ -0,0 +1,16 @@
+package net.ssehub.easy.integration.common.tests;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+/**
+ * Test Suite, which starts all individual test cases.
+ * 
+ * @author El-Sharkawy
+ * @author Holger Eichelberger
+ */
+@RunWith(Suite.class)
+@Suite.SuiteClasses({ FakeTest.class })
+public class AllTests {
+    
+}
