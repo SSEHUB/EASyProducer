7d474fdd9ac0747331e73d081ccfae2586eff80b
Holger Eichelberger <eichelberger@sse.uni-hildesheim.de>
2024-07-17 18:06:16 +0200

    deferred VIL registration, app definition

--- Plugins/EASy-Producer/EASy-Producer.UI/src/net/ssehub/easy/producer/ui/internal/Startup.java
+++ Plugins/EASy-Producer/EASy-Producer.UI/src/net/ssehub/easy/producer/ui/internal/Startup.java
@@ -7,6 +7,7 @@ import org.eclipse.ui.PlatformUI;
 import net.ssehub.easy.basics.logger.EASyLoggerFactory;
 import net.ssehub.easy.basics.logger.EASyLoggerFactory.EASyLogger;
 import net.ssehub.easy.instantiation.core.model.execution.TracerFactory;
+import net.ssehub.easy.instantiation.core.model.vilTypes.TypeRegistry;
 import net.ssehub.easy.producer.eclipse.persistency.ResourcesMgmt;
 import net.ssehub.easy.producer.ui.core.instantiation.GuiTracerFactory;
 
@@ -27,7 +28,9 @@ public class Startup implements IStartup {
                 // I would expect the following line to be part of the startup of EASy core
                 EASyLogger logger = EASyLoggerFactory.INSTANCE.getLogger(Startup.class,
                     "de.uni_hildesheim.sse.easy.ui");
-                logger.info("EASy-Producer 2.0 is up and running...");
+                logger.info("Processing deferred VIL/VTL registrations ...");
+                TypeRegistry.runDeferredRegistrations();
+                logger.info("EASy-Producer is up and running...");
                 ResourcesMgmt.INSTANCE.findPLProjects();
             }
         });
