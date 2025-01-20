86be2a35b2d8033ff0deb6f500f7b01a31453469
Holger Eichelberger <eichelberger@sse.uni-hildesheim.de>
2025-01-20 13:57:08 +0100

    no fast check -> hangup

--- Plugins/IVML/de.uni_hildesheim.sse.ivml/src/de/uni_hildesheim/sse/validation/IvmlValidator.java
+++ Plugins/IVML/de.uni_hildesheim.sse.ivml/src/de/uni_hildesheim/sse/validation/IvmlValidator.java
@@ -9,6 +9,7 @@ import java.net.URI;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.EStructuralFeature;
 import org.eclipse.xtext.validation.Check;
+import org.eclipse.xtext.validation.CheckType;
 
 import de.uni_hildesheim.sse.Config;
 import de.uni_hildesheim.sse.ModelUtility;
@@ -72,7 +73,7 @@ public class IvmlValidator extends AbstractIvmlValidator {
      * 
      * @param unit the variability unit to start tests with
      */
-    @Check
+    @Check(CheckType.NORMAL)
     public void checkModel(VariabilityUnit unit) {
         ValidationUtils.checkModel(unit, callback, Config.isDebuggingEnabled());
     }
