87952dfb5b625321c5c5c8bfb87d278fc3b5ab45
Holger Eichelberger <eichelberger@sse.uni-hildesheim.de>
2026-09-14 09:58:19 +0200

    including cleanOkto.sh

---
+++ EASyBuildProperties/cleanOkto.sh
@@ -0,0 +1,43 @@
+#/bin/bash
+
+IMGS=$(docker images -qa -f 'dangling=true')
+echo "Cleaning up dangling docker images: $IMGS"
+if [ ! -z "$IMGS" ]
+then
+    docker rmi -f "$IMGS"
+fi
+
+IMGS=$(docker images -qa "simplemeshtestingcontainerapp/*")
+echo "Cleaning up IIP-Ecosphere configuration.configuration test images: $IMGS"
+if [ ! -z "$IMGS" ]
+then
+    docker rmi -f "$IMGS" 
+fi
+
+IMGS=$(docker images -qa "allapps/*")
+echo "Cleaning up IIP-Ecosphere allapps test images: $IMGS"
+if [ ! -z "$IMGS" ]
+then
+    docker rmi -f "$IMGS" 
+fi
+
+IMGS=$(docker images -qa "platform/*")
+echo "Cleaning up IIP-Ecosphere platform test images: $IMGS"
+if [ ! -z "$IMGS" ]
+then
+    docker rmi -f "$IMGS" 
+fi
+
+docker system prune -f --volumes
+rm -rf /tmp/tomcat-docbase.*
+rm -rf /tmp/tomcat.*
+rm -rf /tmp/qmProfiling*
+rm -rf /tmp/basyx-temp*
+rm -f /tmp/services.spring*.xml
+rm -f /tmp/platform-*.aasx
+rm -f /tmp/librocksdbjni*.so
+rm -f /tmp/examples-test-*.out
+rm -rf /tmp/.com.google.Chrome.*
+rm -rf /tmp/jansi-*
+rm -rf /tmp/node-compile-cache
+rm -f /tmp/config.cp*
